64891798ad9544a893ac4acea884c1299f2bee75
unknown <nastia60203@gmail.com>
2024-06-21 22:33:18 +0300

    add: image ts file/assets

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,21 +1,9 @@
-import React from "react";
+import Header from "components/Header";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className=" text-6xl"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learh
-        </a>
-      </header>
+      <Header />
     </div>
   );
 }
---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,12 @@
+import Logo from "../../assets/images/logo.png";
+
+const Header = () => {
+  return (
+    <header>
+      <img src={Logo} alt="" />
+      <p className="">hfddhfh</p>
+    </header>
+  );
+};
+
+export default Header;
