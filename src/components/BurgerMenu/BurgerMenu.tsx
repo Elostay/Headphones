f9b839233fa5f2ea05f85d1783434924f54523f9
unknown <nastia60203@gmail.com>
2024-06-24 00:05:00 +0300

    fix

--- src/components/BurgerMenu/BurgerMenu.tsx
+++ src/components/BurgerMenu/BurgerMenu.tsx
@@ -6,17 +6,17 @@ interface BurgerMenuProps {
 const BurgerMenu = ({ isOpen }: BurgerMenuProps) => {
   return (
     <div
-      className={`fixed top-0 right-0 w-full h-full bg-white shadow-lg transform transition-transform duration-300 z-10 bg-[#191919] ${
+      className={`fixed top-0 right-0 w-full h-full  shadow-lg transform transition-transform duration-300 z-10 bg-[#191919] ${
         isOpen ? "translate-x-0" : "translate-x-full"
       }`}
     >
       <div className="py-9 px-5 text-white">
         <img src={Logo} alt="Bose logo" className=" mb-14 w-[100px] h-3" />
         <div className="flex flex-col gap-6">
-          <a href="">Characteristics</a>
-          <a href="">History</a>
-          <a href="">Reviews</a>
-          <a href="">Payment and delivery</a>
+          {/* <a href="#">Characteristics</a> */}
+          {/* <a href="#">History</a> */}
+          {/* <a href="#">Reviews</a> */}
+          {/* <a href="#">Payment and delivery</a> */}
         </div>
       </div>
     </div>
