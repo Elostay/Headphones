3cf9d9f38ccbd96df70ee52f797617123dfe9a2c
unknown <nastia60203@gmail.com>
2024-08-30 14:39:10 +0300

    add: form

--- src/components/Feedback/Feedback.tsx
+++ src/components/Feedback/Feedback.tsx
@@ -9,25 +9,36 @@ const Feedback = () => {
         We'll help you find the right headphones for you.
       </p>
       <form>
-        <input type="text" placeholder="Your name" />
-        <input type="email" placeholder="Your e-mail" />
-        <textarea
-          name="message"
-          placeholder="Send us a message"
-          className=" resize-none"
-        ></textarea>
-        <div className="mb-10">
-          <input type="checkbox" />
-          <p>
-            I agree to the
+        <div className="flex flex-col gap-8">
+          <input
+            type="text"
+            placeholder="Your name"
+            className="pb-4 bg-inherit border-b border-[#2B2B2B] placeholder:text-[#5E5E5E] focus:outline-none"
+          />
+          <input
+            type="email"
+            placeholder="Your e-mail"
+            className="pb-4 bg-inherit border-b border-[#2B2B2B] placeholder:text-[#5E5E5E] focus:outline-none"
+          />
+          <textarea
+            name="message"
+            placeholder="Send us a message"
+            className="resize-none pb-4 bg-inherit border-b border-[#2B2B2B] placeholder:text-[#5E5E5E] focus:outline-none"
+          ></textarea>
+        </div>
+        <div className="mb-10 mt-10">
+          <label className="custom-checkbox">
+            <input type="checkbox" />
+            <span className="checkmark mr-6"></span>I agree to the
             <a href="#" className="underline ml-1">
               privacy policy
             </a>
-          </p>
+          </label>
         </div>
+
         <button
           type="submit"
-          className=" font-semibold text-base px-[120px] py-5 border border-white "
+          className=" font-semibold text-base px-[120px] py-5 border-2  border-[#DF3D3A]"
         >
           SEND
         </button>
